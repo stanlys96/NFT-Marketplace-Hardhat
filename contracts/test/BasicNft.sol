501787db4175ce41ba88c442d5424377ea8e31b1
Stanly Sukmajaya <v.stanly.sukmajaya@adira.co.id>
2022-09-22 14:00:44 +0700

    gud update

--- contracts/test/BasicNft.sol
+++ contracts/test/BasicNft.sol
@@ -6,8 +6,11 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 // "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
 contract BasicNft is ERC721 {
-    string public constant TOKEN_URI =
-        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    string public constant TOKEN_URI_1 = "ipfs://QmaVkBn2tKmjbhphU7eyztbvSQU5EXDdqRyXZtRhSGgJGo";
+
+    string public constant TOKEN_URI_2 = "ipfs://QmYQC5aGZu2PTH8XzbJrbDnvhj3gVs7ya33H9mqUNvST3d";
+
+    string public constant TOKEN_URI_3 = "ipfs://QmZYmH5iDbD6v3U2ixoVAjioSzvWJszDzYdbeCLquGSpVm";
 
     uint256 private s_tokenCounter;
 
@@ -30,7 +33,13 @@ contract BasicNft is ERC721 {
         returns (string memory)
     {
         require(_exists(tokenId), "Token ID does not exist!");
-        return TOKEN_URI;
+        if (tokenId % 3 == 0) {
+            return TOKEN_URI_1;
+        } else if (tokenId % 3 == 1) {
+            return TOKEN_URI_2;
+        } else {
+            return TOKEN_URI_3;
+        }
     }
 
     function getTokenCounter() public view returns (uint256) {
--- scripts/mint.js
+++ scripts/mint.js
@@ -3,43 +3,27 @@ const { moveBlocks } = require('../utils/move-blocks');
 
 const NFT_PRICE = ethers.utils.parseEther('0.1');
 
-async function mintAndList() {
+async function mint() {
   const [nftMarketplace, basicNft] = await Promise.all([
     ethers.getContract('NftMarketplace'),
     ethers.getContract('BasicNft'),
   ]);
 
+  console.log(`NFT Address: ${basicNft.address}`);
+
   const mintTx = await basicNft.mintNft();
   const mintTxReceipt = await mintTx.wait(1);
 
-  // const nftMarketplace = await ethers.getContract('NftMarketplace');
-  // const basicNft = await ethers.getContract('BasicNft');
-
-  // const mintTx = await basicNft.mintNft();
-  // const mintTxReceipt = await mintTx.wait(1);
   const tokenId = mintTxReceipt.events[0].args.tokenId.toString();
 
   console.log(`Token ID: ${tokenId} minted!`);
-  const approveTx = await basicNft.approve(nftMarketplace.address, tokenId);
-  await approveTx.wait(1);
-
-  console.log('Item approved for marketplace!');
-
-  const listItemTx = await nftMarketplace.listItem(
-    basicNft.address,
-    tokenId,
-    NFT_PRICE
-  );
-  await listItemTx.wait(1);
-
-  console.log('Item listed!');
 
   if (network.config.chainId == 31337) {
     await moveBlocks(2, (sleepAmount = 1000));
   }
 }
 
-mintAndList()
+mint()
   .then(() => process.exit(0))
   .catch((e) => {
     console.log(e);
